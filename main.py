9ceb87f514cb19d612a0fa92e94941f2fd1755a8
Kellen1717 <kellenbwilliams@icloud.com>
2020-07-07 16:54:06 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+while True:
+  print ("Welcome to the console of Kellen.")
+  print ("What would you like to know about me.")
+  me =input (" A - Where I was born B - What is my favorit food C - What is my favorit sport to play")
+  if(me == "A"  ) :
+    print (" Portland Oregon ")
+  elif(me == "B"):
+    print (" burrito ")
+  elif (me == "C"):
+    print ("track and feild")
